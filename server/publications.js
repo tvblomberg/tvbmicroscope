2b693c31b1e44f939156c679813c7896570b7799
Thomas Blomberg <thomasvblomberg@Thomass-MBP.roc.mn.charter.com>
2016-03-27 17:28:24 -0500

    Microscope app

---
+++ server/publications.js
@@ -0,0 +1,19 @@
+Meteor.publish('posts', function(options) {
+   return Posts.find({}, options); 
+});
+
+Meteor.publish('singlePost', function(id) {
+    check(id, String);
+    
+    return Posts.find(id);
+});
+
+Meteor.publish('comments', function(postId) {
+    check(postId, String);
+    
+    return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+    return Notifications.find({userId: this.userId, read: false});
+});
